46660569c3ffca456c897e3066f25817e3ff8b15
2004_Aniket_Datta <116451512+RamNarayandatta@users.noreply.github.com>
2023-08-14 10:58:43 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+function updateTime(){
+  const now=new Date;
+  let Hours=now.getHours();
+  let minutes=now.getMinutes();
+  let seconds=now.getSeconds();
+  let ampm=Hours>=12 ? "P.M" : "A.M";
+  Hours= Hours% 12
+  Hours=Hours?Hours:12
+  Hours=Hours < 10 ? "0"+ Hours: Hours;
+  minutes=minutes < 10 ? "0"+ minutes: minutes;
+  seconds=seconds < 10 ? "0"+ seconds: seconds;
+  document.getElementById("Hours").textContent = Hours;
+  document.getElementById("minutes").textContent=minutes;
+  document.getElementById("seconds").textContent=seconds;
+  document.getElementById("ampm").textContent=ampm;
+}
+setInterval(updateTime,1000);
+updateTime();
